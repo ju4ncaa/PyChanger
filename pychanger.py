5e778e31f7b3bb4a7d79fdea93afa076880e0e95
Juan Carlos Rodríguez <jcrr.developer@gmail.com>
2023-11-28 15:00:34 +0100

    Update pychanger.py

--- pychanger.py
+++ pychanger.py
@@ -4,6 +4,7 @@
 # Módulos
 import subprocess
 import re
+from platform import system
 from time import sleep
 from sys import exit
 from colorama import Fore
@@ -51,57 +52,61 @@ def select_interface():
 #MAC Spoofing
 def mac_spoofing():
     global interface
-    interface_mac = subprocess.check_output(f"macchanger --show {interface} | awk '{{print $3}}' | head -n 1", shell=True, text=True).strip().lower()
-    subprocess.call("clear", shell=True)
-    banner()
-    print(f"\n\n{YELLOW}[*]{RESET} Selecciona una opción para configurar la interfaz:")
-    print(f"\n{YELLOW}1.{RESET} Imprimir la dirección MAC actual")
-    print(f"{YELLOW}2.{RESET} Establecer una dirección MAC personalizada")
-    print(f"{YELLOW}3.{RESET} Establecer una dirección MAC totalmente aleatoria")
-    print(f"{YELLOW}4.{RESET} Restaurar a la dirección MAC original")
-    option = input("\n> ")
-    
-    if option == "1":
-        print(f"\n{GREEN}[*]{RESET} Dirección MAC actual de la interfaz {interface}: {YELLOW}{interface_mac}{RESET}")
-        
-    elif option == "2":
-        mac_pattern = re.compile(r"^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$")
-        input_mac = input(f"\n{YELLOW}[*]{RESET} Introduce la dirección MAC nueva: ").strip().lower()
-        
-        if input_mac != interface_mac and mac_pattern.match(input_mac):
-            print(f"\n{YELLOW}[*]{RESET} Estableciendo dirección MAC {YELLOW}{input_mac}{RESET} en la interfaz {YELLOW}{interface}{RESET} ...")
-            subprocess.call(f"macchanger --mac={input_mac} {interface} > /dev/null 2>&1", shell=True)
-            sleep(1)
-            new_mac = subprocess.check_output(f"macchanger --show {interface} | awk '{{print $3}}' | head -n 1", shell=True, text=True).strip().lower()
-            print(f"\n{GREEN}[*]{RESET} Dirección MAC modificada exitosamente!")
-            print(f"\n{CYAN}[*]{RESET} Dirección MAC antigua: {YELLOW}{interface_mac}{RESET}")
-            print(f"\n{CYAN}[*]{RESET} Dirección MAC actual: {YELLOW}{new_mac}{RESET}")
-        else:
-            if input_mac == interface_mac:
-                print(f"\n{RED}[!]{RESET} La nueva dirección MAC es la misma que la dirección MAC actual.")
+    while True:
+        interface_mac = subprocess.check_output(f"macchanger --show {interface} | awk '{{print $3}}' | head -n 1", shell=True, text=True).strip().lower()
+        subprocess.call("clear", shell=True)
+        banner()
+        print(f"\n\n{YELLOW}[*]{RESET} Selecciona una opción para configurar la interfaz:")
+        print(f"\n{YELLOW}1.{RESET} Imprimir la dirección MAC actual")
+        print(f"{YELLOW}2.{RESET} Establecer una dirección MAC personalizada")
+        print(f"{YELLOW}3.{RESET} Establecer una dirección MAC totalmente aleatoria")
+        print(f"{YELLOW}4.{RESET} Restaurar a la dirección MAC original")
+        option = input("\n> ")
+
+        if option == "1":
+            print(f"\n{GREEN}[*]{RESET} Dirección MAC actual de la interfaz {interface}: {YELLOW}{interface_mac}{RESET}")
+
+        elif option == "2":
+            mac_pattern = re.compile(r"^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$")
+            input_mac = input(f"\n{YELLOW}[*]{RESET} Introduce la dirección MAC nueva: ").strip().lower()
+
+            if input_mac != interface_mac and mac_pattern.match(input_mac):
+                print(f"\n{YELLOW}[*]{RESET} Estableciendo dirección MAC {YELLOW}{input_mac}{RESET} en la interfaz {YELLOW}{interface}{RESET} ...")
+                subprocess.call(f"macchanger --mac={input_mac} {interface} > /dev/null 2>&1", shell=True)
+                sleep(1)
+                new_mac = subprocess.check_output(f"macchanger --show {interface} | awk '{{print $3}}' | head -n 1", shell=True, text=True).strip().lower()
+                print(f"\n{GREEN}[*]{RESET} Dirección MAC modificada exitosamente!")
+                print(f"\n{CYAN}[*]{RESET} Dirección MAC antigua: {YELLOW}{interface_mac}{RESET}")
+                print(f"\n{CYAN}[*]{RESET} Dirección MAC actual: {YELLOW}{new_mac}{RESET}")
             else:
-                print(f"\n{RED}[!]{RESET} Formato de dirección MAC incorrecto")
+                if input_mac == interface_mac:
+                    print(f"\n{RED}[!]{RESET} La nueva dirección MAC es la misma que la dirección MAC actual.")
+                else:
+                    print(f"\n{RED}[!]{RESET} Formato de dirección MAC incorrecto")
+                exit(1)
+        elif option == "3":
+             print(f"\n{YELLOW}[*]{RESET} Estableciendo dirección MAC aleatoria en la interfaz {YELLOW}{interface}{RESET} ...")
+             subprocess.call(f"macchanger --random {interface} > /dev/null 2>&1", shell=True)
+             sleep(1)
+             new_mac = subprocess.check_output(f"macchanger --show {interface} | awk '{{print $3}}' | head -n 1", shell=True, text=True).strip().lower()
+             print(f"\n{GREEN}[*]{RESET} Dirección MAC modificada exitosamente!")
+             print(f"\n{CYAN}[*]{RESET} Dirección MAC antigua: {YELLOW}{interface_mac}{RESET}")
+             print(f"\n{CYAN}[*]{RESET} Dirección MAC actual: {YELLOW}{new_mac}{RESET}")
+        elif option == "4":
+            print(f"\n{YELLOW}[*]{RESET} Restaurando la dirección MAC original en la interfaz {YELLOW}{interface}{RESET} ...")
+            subprocess.call(f"macchanger --show {interface} --another > /dev/null 2>&1", shell=True)
+            sleep(1)
+            restored_mac = subprocess.check_output(f"macchanger --show {interface} | awk '{{print $3}}' | head -n 1", shell=True, text=True).strip().lower()
+            print(f"\n{GREEN}[*]{RESET} Dirección MAC actual restaurada exitosamente!")
+            print(f"\n{CYAN}[*]{RESET} Dirección MAC original: {YELLOW}{restored_mac}{RESET}")
+        else:
+            print(f"{RED}[!]{RESET} Opción inválida")
             exit(1)
-    elif option == "3":
-         print(f"\n{YELLOW}[*]{RESET} Estableciendo dirección MAC aleatoria en la interfaz {YELLOW}{interface}{RESET} ...")
-         subprocess.call(f"macchanger --random {interface} > /dev/null 2>&1", shell=True)
-         sleep(1)
-         new_mac = subprocess.check_output(f"macchanger --show {interface} | awk '{{print $3}}' | head -n 1", shell=True, text=True).strip().lower()
-         print(f"\n{GREEN}[*]{RESET} Dirección MAC modificada exitosamente!")
-         print(f"\n{CYAN}[*]{RESET} Dirección MAC antigua: {YELLOW}{interface_mac}{RESET}")
-         print(f"\n{CYAN}[*]{RESET} Dirección MAC actual: {YELLOW}{new_mac}{RESET}")
-    elif option == "4":
-        print(f"\n{YELLOW}[*]{RESET} Restaurando la dirección MAC original en la interfaz {YELLOW}{interface}{RESET} ...")
-        subprocess.call(f"macchanger --show {interface} --another > /dev/null 2>&1", shell=True)
-        sleep(1)
-        restored_mac = subprocess.check_output(f"macchanger --show {interface} | awk '{{print $3}}' | head -n 1", shell=True, text=True).strip().lower()
-        print(f"\n{GREEN}[*]{RESET} Dirección MAC actual restaurada exitosamente!")
-        print(f"\n{CYAN}[*]{RESET} Dirección MAC original: {YELLOW}{restored_mac}{RESET}")
-    else:
-        print(f"{RED}[!]{RESET} Opción inválida")
-        exit(1)
-        
-    
+        subprocess.call(f"ifconfig {interface} up > /dev/null 2>&1", shell=True)
+        user_choice = input(f"\n{YELLOW}[*]{RESET} ¿Quieres realizar otra operación? (s/n): ")
+        if user_choice.lower() != "s":
+            print(f"\n{RED}[!]{RESET} Saliendo...")
+            exit(1)
 
 #Funcion principal
 def main():
@@ -115,7 +120,10 @@ def main():
 
 #Programa principal
 if __name__ == "__main__":
-    try:
-        main()
-    except KeyboardInterrupt:
-        print(f"\n\n{RED}[!]{RESET} Saliendo...")
+    if system() == "Windows":
+        print(f"\n{RED}[!]{RESET} El script no soporta el sistema operativo Windows\n")
+    else:
+        try:
+            main()
+        except KeyboardInterrupt:
+            print(f"\n\n{RED}[!]{RESET} Saliendo...")
