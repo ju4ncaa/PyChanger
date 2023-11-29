b34ac635200cd454328f18d02fdd44a15048b7ea
Juan Carlos Rodríguez <jcrr.developer@gmail.com>
2023-11-29 17:37:56 +0100

    Update pychanger.py

--- pychanger.py
+++ pychanger.py
@@ -7,14 +7,13 @@ import re
 from platform import system
 from time import sleep
 from sys import exit
-from colorama import Fore
 
 # Paleta de colores
-GREEN = Fore.GREEN
-RED = Fore.RED
-CYAN = Fore.CYAN
-YELLOW = Fore.YELLOW
-RESET = Fore.RESET
+GREEN = "\033[92m"
+RED = "\033[91m"
+CYAN = "\033[96m"
+YELLOW = "\033[93m"
+RESET = "\033[0m"
 
 #Banner
 def banner():
